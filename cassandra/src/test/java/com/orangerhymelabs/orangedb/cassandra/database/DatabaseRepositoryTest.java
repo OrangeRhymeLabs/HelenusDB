65bbd625f2626aef9c32eafdd7fc5d2259fb2930
Todd Fredrich <tfredrich@gmail.com>
2015-05-10 17:37:08 -0600

    Testing async/callback behavior.

--- cassandra/src/test/java/com/orangerhymelabs/orangedb/cassandra/database/DatabaseRepositoryTest.java
+++ cassandra/src/test/java/com/orangerhymelabs/orangedb/cassandra/database/DatabaseRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.orangerhymelabs.orangedb.cassandra.database;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.thrift.transport.TTransportException;
@@ -11,13 +14,14 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.ResultSetFuture;
 import com.orangerhymelabs.orangedb.cassandra.CassandraManager;
 import com.orangerhymelabs.orangedb.cassandra.KeyspaceSchema;
+import com.orangerhymelabs.orangedb.persistence.ResultCallback;
 
 public class DatabaseRepositoryTest
 {
+	private static final int CALLBACK_TIMEOUT = 2000;
+
 	private static KeyspaceSchema keyspace;
 	private static DatabaseRepository databases;
 
@@ -74,5 +78,119 @@ public class DatabaseRepositoryTest
 		// Re-Read
 		Database result3 = databases.read(entity.getId());
 		assertNull(result3);
+	}
+
+	@Test
+	public void shouldCreateDatabaseAsyncronously()
+	throws InterruptedException
+	{
+		Database entity = new Database();
+		entity.name("db2");
+		entity.description("another test database");
+		DatabaseCallback callback = new DatabaseCallback();
+
+		// Create
+		databases.createAsync(entity, callback);
+		waitOn(callback);
+
+		assertNull(callback.throwable());
+//		assertEquals(entity, callback.database());
+
+		// Read
+		callback.clear();
+		databases.readAsync(entity.getId(), callback);
+		waitOn(callback);
+
+		assertEquals(entity, callback.database());
+
+		// Update
+		callback.clear();
+		entity.description("an updated test database");
+		databases.updateAsync(entity, callback);
+		waitOn(callback);
+
+		assertNull(callback.throwable());
+//		assertEquals(entity, callback.database());
+
+		// Re-Read
+		callback.clear();
+		databases.readAsync(entity.getId(), callback);
+		waitOn(callback);
+
+		Database result2 = callback.database();
+		assertEquals(entity, result2);
+		assertNotEquals(result2.createdAt(), result2.updatedAt());
+		assertNotNull(result2.createdAt());
+		assertNotNull(result2.updatedAt());
+
+		// Delete
+		callback.clear();
+		databases.deleteAsync(entity.getId(), callback);
+		waitOn(callback);
+
+		assertNull(callback.throwable());
+
+		// Re-Read
+		callback.clear();
+		databases.readAsync(entity.getId(), callback);
+		waitOn(callback);
+		
+		assertTrue(callback.isEmpty());
+	}
+
+	private void waitOn(DatabaseCallback callback)
+	throws InterruptedException
+    {
+		synchronized(callback)
+		{
+			callback.wait(CALLBACK_TIMEOUT);
+		}
+    }
+
+	private class DatabaseCallback
+	implements ResultCallback<Database>
+	{
+		private Database database;
+		private Throwable throwable;
+
+		@Override
+        public void onSuccess(Database result)
+        {
+			this.database = result;
+			alert();
+        }
+
+		@Override
+        public void onFailure(Throwable t)
+        {
+			this.throwable = t;
+			alert();
+        }
+
+		private synchronized void alert()
+		{
+			notifyAll();
+		}
+
+		public void clear()
+		{
+			this.database = null;
+			this.throwable = null;
+		}
+
+		public boolean isEmpty()
+		{
+			return (database == null && throwable == null);
+		}
+
+		public Database database()
+		{
+			return database;
+		}
+
+		public Throwable throwable()
+		{
+			return throwable;
+		}
 	}
 }
